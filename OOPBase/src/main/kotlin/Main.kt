f368daf0fba9f11bd3f22c784719a35859d57b23
NikitaFrol <144579830+NikitaFrol@users.noreply.github.com>
2023-09-17 12:55:03 +0600

    if tasks

---
+++ OOPBase/src/main/kotlin/Main.kt
@@ -0,0 +1,97 @@
+fun main(args: Array<String>) {
+   var x = 6
+    var y = 8
+
+    if (y != 0) {
+        print(x / y)
+    } else {
+        println ("делит на ноль нельзя ")
+    }
+
+    println ("программа закончина ")
+
+
+
+
+    var p = 3
+    var b = 6
+    var fn = p + b
+    if (fn > 0) {
+        println ("больше ноля ")
+    }else {
+    println ("меньше ноля ")
+    }
+
+    val month = 12
+    if (month == 12) {
+        println("december")
+    }
+    else if (month == 11) {
+        println("november")
+    }
+    else if (month == 10) {
+            println("october")
+    }
+    else if (month == 9 ) {
+        println("september")
+    }
+    else if (month == 8 ){
+        println("avgisy")
+    }else {
+
+    }
+
+    }
+// задачи по теме if-else
+fun ifFirstTask() {
+    var x = 6
+    var y = 8
+
+    if (y != 0) {
+        print(x / y)
+    } else {
+        println ("делит на ноль нельзя ")
+    }
+
+    println ("программа закончина ")
+}
+
+fun ifSecondTask() {
+    var p = 3
+    var b = 6
+    var fn = p + b
+    if (fn > 0) {
+        println ("больше ноля ")
+    }else {
+        println ("меньше ноля ")
+    }
+}
+
+fun ifThirdTask() {
+    val month = 12
+    if (month == 12) {
+        println("december")
+    }
+    else if (month == 11) {
+        println("november")
+    }
+    else if (month == 10) {
+        println("october")
+    }
+    else if (month == 9 ) {
+        println("september")
+    }
+    else if (month == 8 ){
+        println("avgisy")
+    }else {
+}
+
+
+
+
+
+
+}
+
+
+

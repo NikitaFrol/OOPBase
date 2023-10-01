4f14ec9ae4b7afcf79fb07dfdf7c5ed3b028cf06
NikitaFrol <144579830+NikitaFrol@users.noreply.github.com>
2023-10-01 12:50:21 +0600

    lesson 3

--- OOPBase/src/main/kotlin/Main.kt
+++ OOPBase/src/main/kotlin/Main.kt
@@ -1,47 +1,26 @@
 fun main(args: Array<String>) {
-   var x = 6
-    var y = 8
+   val number= listOf<Int>(13,65,3,67,-3,1)
+    val x = findMax(number)
+    println(x)
+}
 
-    if (y != 0) {
-        print(x / y)
-    } else {
-        println ("делит на ноль нельзя ")
+fun findMax (inputList: List<Int>): Int {
+    var max = Int.MIN_VALUE
+    for (i in inputList.indices) {
+        if (max < inputList[i]) {
+           max = inputList[i]
+        }
     }
+    return max
+}
 
-    println ("программа закончина ")
-
-
-
-
-    var p = 3
-    var b = 6
-    var fn = p + b
-    if (fn > 0) {
-        println ("больше ноля ")
-    }else {
-    println ("меньше ноля ")
+fun printList (inputList: List <Any>) {
+    for (i in inputList.indices){
+        println(inputList[i])
     }
+}
 
-    val month = 12
-    if (month == 12) {
-        println("december")
-    }
-    else if (month == 11) {
-        println("november")
-    }
-    else if (month == 10) {
-            println("october")
-    }
-    else if (month == 9 ) {
-        println("september")
-    }
-    else if (month == 8 ){
-        println("avgisy")
-    }else {
 
-    }
-
-    }
 // задачи по теме if-else
 fun ifFirstTask() {
     var x = 6
@@ -93,5 +72,24 @@ fun ifThirdTask() {
 
 }
 
+fun iftask4(){
+    val month = 12
+    if (month == 12) {
+        println("december")
+    }
+    else if (month == 11) {
+        println("november")
+    }
+    else if (month == 10) {
+        println("october")
+    }
+    else if (month == 9 ) {
+        println("september")
+    }
+    else if (month == 8 ){
+        println("avgisy")
+    }else {
 
+    }
+}
 
